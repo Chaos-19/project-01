9680ba2a97fae1ddf39b3acb511a267dda0445e3
nazrawi777 <151239515+nazrawi777@users.noreply.github.com>
2023-12-28 10:28:41 -0800

    adding utils comp ..

---
+++ src/constants/index.js
@@ -0,0 +1,29 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+
+export { services, technologies, experiences, testimonials, projects };
