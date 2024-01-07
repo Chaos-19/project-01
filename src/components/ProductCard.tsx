adf93e08603e8300febd8fab9800bf4c19ede3a3
Chaos-19 <kalgetachew375@gmail.com>
2024-01-07 15:17:21 +0300

    update card info

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -13,33 +13,33 @@ interface Props {
     };
 }
 
+
+
 const ProductCard = (props: Props) => {
     const { name, price, image, tage } = props;
 
     console.log(tage);
 
     return (
-        <div className="relative flex flex-col bg-white shadow-xl md:pb-2 rounded">
-            <div className="order-3  md:relative absolute top-2.5 left-2.5 md:top-0 md:left-0">
-                <div className="md:absolute md:-top-8 md:p-2.5 md:w-full">
-                    {price?.discountPercent && (
-                        <span className="text-xs px-1 py-0.5 bg-sky-500 rounded">
-                            {price?.discountPercent}%
-                        </span>
-                    )}
-                    {tage?.isNew && (
-                        <span className="text-xs text-white capitalize p-1 bg-red-700 rounded">
-                            New
-                        </span>
-                    )}
-                </div>
+        <div className="relative flex flex-col bg-white shadow-xl  rounded">
+            <div className="inset-x-0 h-fit absolute pt-2.5 md:pt-0 md:bottom-2.5 left-2.5">
+                {price?.discountPercent && (
+                    <span className="text-xs px-1 py-0.5 bg-sky-500 rounded">
+                        {price?.discountPercent}%
+                    </span>
+                )}
+                {tage?.isNew && (
+                    <span className="text-xs text-white capitalize p-1 bg-red-700 rounded">
+                        New
+                    </span>
+                )}
             </div>
             <img
                 src={image}
                 alt={`${name} image`}
                 className="order-2 bg-cover"
             />
-            <div className="p-2.5 md:pb-0 order-3 md:order-1">
+            <div className="float-left p-2.5 md:pb-0 order-3 md:order-1">
                 <h3 className="text-md font-mono uppercase text-gray-600 font-black">
                     {name}
                 </h3>
