12ae2e5f103e9013f457cf3801ffa5dca787fe90
Chaos-19 <kalgetachew375@gmail.com>
2024-01-11 10:36:24 +0300

    update footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -10,11 +10,14 @@ const Footer = (/*props: Props*/) => {
         <div className="w-full flex flex-col justify-center">
             <div className="flex justify-center items-center bg-white">
                 <div className="mt-5 w-full">
-                    <h2 className="text-2xl font-medium text-gray-700 uppercase flex items-center justify-center gap-3">
+                    <h2 className="text-xl font-medium text-gray-500 uppercase flex items-end justify-center gap-3">
                         <span>follow us</span>
-                        <Instagram />
+                        <Instagram size={32} />
                         <span>Instagram</span>
                     </h2>
+                    <p className="text-center text-sm text-gray-400">
+                        @elfegnefurnishing
+                    </p>
                 </div>
             </div>
         </div>
