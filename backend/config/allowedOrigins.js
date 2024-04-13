5edcbc787bd7483340b5c52007b6c4c50ed84a1b
Kalkidan Getachew <110995815+Chaos-19@users.noreply.github.com>
2024-04-13 04:12:19 -0700

    Update allowedOrigins.js
    update list of allowed origin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,3 @@
 module.exports = allowedOrigins = [
-    "http://127.0.0.1:5500",
-    "http://localhost:8080",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:4173"
+    "https://project-01-2.onrender.com",
 ];
