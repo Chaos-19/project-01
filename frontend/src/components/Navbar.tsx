2473060fbac50a848fdb14e0ec0dc7a2594cc1cc
Kalkidan Getachew <110995815+Chaos-19@users.noreply.github.com>
2024-04-13 04:57:24 -0700

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -38,7 +38,7 @@ const Navbar = (props: { navLinks: {}[] }) => {
                         <img
                             src={logo}
                             alt="brand logo"
-                            className="w-5 h-5"
+                            className="w-10 h-10"
                         />
                     </Link>
 
